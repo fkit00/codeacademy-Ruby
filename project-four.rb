ca8c0611661215d4a5b31c595ae0fa67d49731f1
Fiona <100845392+fkit00@users.noreply.github.com>
2022-11-17 14:30:20 +0000

    Create project-four.rb

---
+++ project-four.rb
@@ -0,0 +1,11 @@
+# This was made to work with arrays and hashes 
+
+puts "Text please"
+text = gets.chomp
+words = text.split(" ")
+
+frequencies = Hash.new(0)
+words.each{|word| frequencies[word]+=1}
+frequencies = frequencies.sort_by{ |word, count| count}
+frequencies.reverse!
+frequencies.each{|word, count| puts word + " " + count.to_s}
