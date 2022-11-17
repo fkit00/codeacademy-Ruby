ee78b96a269c663dc3bf9eb8a0119fd71848bcdf
Fiona <100845392+fkit00@users.noreply.github.com>
2022-11-17 14:35:34 +0000

    Create project-eight.rb

---
+++ project-eight.rb
@@ -0,0 +1,20 @@
+# this project was focused on blocks and lambda's
+
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+
+
+first_half = lambda { |a,b| b<"M" }
+
+a_to_m = crew.select(&first_half)
+
+puts a_to_m
