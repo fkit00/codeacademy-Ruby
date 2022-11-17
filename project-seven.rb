a09e89a950614d68e7825fd59ede116193b3f3f0
Fiona <100845392+fkit00@users.noreply.github.com>
2022-11-17 14:34:09 +0000

    Create project-seven.rb

---
+++ project-seven.rb
@@ -0,0 +1,11 @@
+# this project focused on refactoring code
+
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+  return "n must be an integer." unless n.is_a? Integer
+  return "n must be greater than 0." unless n > 0
+  Prime.first n
+end
+
+puts first_n_primes(10)
